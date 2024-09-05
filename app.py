8edb56006e84b3f69e8897d33ef22f3b2197c5d0
Kausik Pattanaik <kausikpattanaik2002@gmail.com>
2024-09-05 20:52:47 +0530

    Adding changes to app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ try:
 
     # Load
     df.to_csv('transformed_data.csv', index=False)
-    log_message(f'ETL job completed at {etl_timestamp} and data is saved to transformed_data.csv')
+    log_message(f'ETL job completed at {etl_timestamp} and data is saved to a transformed_data.csv file')
 
 except Exception as e:
     log_message(f'ETL job failed with error: {e}')
